658daba7bbd03c77beb77450f9b139b4bf734623
Evan Clevenger <evanclevenger@gmail.com>
2025-10-26 20:06:29 -0500

    added header

--- coding-challenge/src/App.js
+++ coding-challenge/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import logo from "./logo_white.png";
 
 function App() {
+  const loremIpsum =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={logo} alt="logo" className="logo" />
+        <div className="header-options">
+          <p>Benefits</p>
+          <p>Safety</p>
+          <p>Policies</p>
+        </div>
       </header>
+      <div className="benefits-cards">
+        <ul>
+          <li className="card-header">PPO Plan</li>
+          <li className="card-paragraph">{loremIpsum}</li>
+        </ul>
+      </div>
     </div>
   );
 }
